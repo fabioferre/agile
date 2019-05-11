03beca88a8e5461bdd1d9d622370324ffc94960e
kevin <vitorkortez@gmail.com>
2019-05-11 16:45:56 -0300

    update - interceptor
    identação

--- src/app/service/interceptor.service.ts
+++ src/app/service/interceptor.service.ts
@@ -5,49 +5,49 @@ import { Observable, of, from } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private storage: Storage) { }
+    constructor(private storage: Storage) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let promise = this.storage.get('user');
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let promise = this.storage.get('user');
 
-    return from(promise).pipe(mergeMap(user => {
+        return from(promise).pipe(mergeMap(user => {
 
-      let clonedReq = this.addToken(request, user);
-      return next.handle(clonedReq);
-    }));
-  }
+            let clonedReq = this.addToken(request, user);
+            return next.handle(clonedReq);
+        }));
+    }
 
-  private addToken(request: HttpRequest<any>, user: any) {
-    if (user) {
-      let clone: HttpRequest<any>;
-      clone = request.clone({
-        setHeaders: {
-          "Content-Type": "application/json",
-          "cache-control": "no-cache",
-           Accept: "application/json",
-           key: `${user.key}`
+    private addToken(request: HttpRequest<any>, user: any) {
+        if (user) {
+            let clone: HttpRequest<any>;
+            clone = request.clone({
+                setHeaders: {
+                    "Content-Type": "application/json",
+                    "cache-control": "no-cache",
+                    Accept: "application/json",
+                    key: `${user.key}`
+                }
+
+            });
+            return clone;
+        } else {
+            let clone: HttpRequest<any>;
+            clone = request.clone({
+                setHeaders: {
+                    "Content-Type": "application/json",
+                    "cache-control": "no-cache",
+                    Accept: "application/json"
+                }
+
+            });
+            return clone;
+
         }
-
-      });
-      return clone;
-    }else{
-      let clone: HttpRequest<any>;
-      clone = request.clone({
-        setHeaders: {
-          "Content-Type": "application/json",
-          "cache-control": "no-cache",
-           Accept: "application/json"
-        }
-
-      });
-      return clone;
 
     }
-    
-  }
 
 }

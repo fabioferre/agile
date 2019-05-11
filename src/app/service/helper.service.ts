7362a42586333db8ef6c4eee331a81352a04b4bb
Fabio <fabio.ferreira@w1finance.com.br>
2019-05-11 16:41:39 -0300

    intercept

--- src/app/service/helper.service.ts
+++ src/app/service/helper.service.ts
@@ -1,14 +1,69 @@
 import { Injectable } from '@angular/core';
+import { ToastController, LoadingController, ModalController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
   private apiURL = 'http://tagmus.com.br/api';
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private loading: LoadingController,
+    private modalCtrl: ModalController,
+     ) { }
 
 
   get url() {
     return this.apiURL;
   }
+
+  message(message?) {
+    if (message.error) {
+      for (let i in message.error.errors) {
+
+        this.toast("danger", "Atenção: " + message.error.errors[i]);
+      }
+    } else {
+      this.toast("success", message);
+    }
+
+  }
+
+  async toast(status, message) {
+    const toast = await this.toastCtrl.create(
+      {
+        message: message,
+        position: "top",
+        color: status,
+        duration: 3000,
+        showCloseButton: true
+      });
+
+    toast.present().then(() => this.loading.dismiss());
+
+  }
+
+  async load(status?) {
+    if (status === false) {
+      this.loading.dismiss();
+    } else {
+      let loading = await this.loading.create({
+        message: "Processando..."
+      });
+      return await loading.present();
+    }
+
+
+  }
+
+  async modalDismiss(status?, then?) {
+    if (status) {
+      return await this.modalCtrl.dismiss(status);
+    } else if (then) {
+      this.modalCtrl.dismiss().then(() => { then });
+    }
+    return await this.modalCtrl.dismiss();
+
+  }
+
 }
--- src/app/service/interceptor.service.ts
+++ src/app/service/interceptor.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { Observable, of, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorService {
+export class InterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let promise = this.storage.get('user');
+
+    return from(promise).pipe(mergeMap(user => {
+
+      let clonedReq = this.addToken(request, user);
+      return next.handle(clonedReq);
+    }));
+  }
+
+  private addToken(request: HttpRequest<any>, user: any) {
+    if (user) {
+      let clone: HttpRequest<any>;
+      clone = request.clone({
+        setHeaders: {
+          "Content-Type": "application/json",
+          "cache-control": "no-cache",
+           Accept: "application/json",
+           key: `${user.key}`
+        }
+
+      });
+      return clone;
+    }else{
+      let clone: HttpRequest<any>;
+      clone = request.clone({
+        setHeaders: {
+          "Content-Type": "application/json",
+          "cache-control": "no-cache",
+           Accept: "application/json"
+        }
+
+      });
+      return clone;
+
+    }
+    
+  }
+
 }

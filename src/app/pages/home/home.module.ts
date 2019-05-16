7b03a1bf4e9c92a3ebfabcb7e95233093ae88d34
Fabio <fabio.ferreira@w1finance.com.br>
2019-05-16 11:40:55 -0300

    Update home.module.ts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -9,7 +9,6 @@ import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
 import { PainelPedidoComponent } from './painel-pedido/painel-pedido.component';
-import { CupomFiscalComponent } from './painel-pedido/cupom-fiscal/cupom-fiscal.component';
 import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 import { ProdutoService } from '../produtos/produto.service';
 

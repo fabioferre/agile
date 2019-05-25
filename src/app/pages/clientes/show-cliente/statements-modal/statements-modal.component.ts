c4cf08199d7e02015069d3fac0fa2779766da0e9
Fabio <fabio.ferreira@w1finance.com.br>
2019-05-25 20:58:31 -0300

    cliete

--- src/app/pages/clientes/show-cliente/statements-modal/statements-modal.component.ts
+++ src/app/pages/clientes/show-cliente/statements-modal/statements-modal.component.ts
@@ -1,17 +1,24 @@
 import { PedidosService } from './../../../pedidos/pedidos.service';
 import { Component, OnInit } from '@angular/core';
+import { NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-statements-modal',
   templateUrl: './statements-modal.component.html',
-  styleUrls: ['./statements-modal.component.scss'],
+  styleUrls: [
+    './statements-modal.component.scss',
+    './../../../pedidos/mostra-pedido/mostra-pedido.component.scss',
+    './../../../home/painel-pedido/painel-pedido.component.scss'
+  ],
 })
 export class StatementsModalComponent implements OnInit {
   public order: any;
-  constructor(public  pedidosService: PedidosService) { }
+  constructor(public pedidosService: PedidosService,
+    public params : NavParams) { }
 
   ngOnInit() {
-    this.pedidosService.getById(237).subscribe((order) => {
+ 
+    this.pedidosService.getById(this.params.get('order_id')).subscribe((order) => {
       this.order = order;
     });
   }

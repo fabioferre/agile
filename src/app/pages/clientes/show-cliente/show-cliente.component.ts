7afebc63619bc1f0a8f90a7d8c4aca0462e5213f
kevin <vitorkortez@gmail.com>
2019-05-26 13:46:52 -0300

    upping - botões de entrada e retirada de saldo

--- src/app/pages/clientes/show-cliente/show-cliente.component.ts
+++ src/app/pages/clientes/show-cliente/show-cliente.component.ts
@@ -5,93 +5,93 @@ import { AlertController } from '@ionic/angular';
 
 
 @Component({
-  selector: 'app-show-cliente',
-  templateUrl: './show-cliente.component.html',
-  styleUrls: ['./show-cliente.component.scss'],
+	selector: 'app-show-cliente',
+	templateUrl: './show-cliente.component.html',
+	styleUrls: ['./show-cliente.component.scss'],
 })
 export class ShowClienteComponent implements OnInit {
-  public orders;
-  constructor(public clientesService: ClientesService,
-    private activedRoute: ActivatedRoute,
-    private alertCtrl: AlertController) { }
+	public orders;
+	constructor(public clientesService: ClientesService,
+		private activedRoute: ActivatedRoute,
+		private alertCtrl: AlertController) { }
 
-  ngOnInit() {
-    if (!this.clientesService.clientToShow) {
-      this.clientesService.getById(this.activedRoute.snapshot.paramMap.get('id')).subscribe((client) => {
-        this.clientesService.clientToShow = client;
-      });
-    }
+	ngOnInit() {
+		if (!this.clientesService.clientToShow) {
+			this.clientesService.getById(this.activedRoute.snapshot.paramMap.get('id')).subscribe((client) => {
+				this.clientesService.clientToShow = client;
+			});
+		}
 
-    this.clientesService.getSum(this.activedRoute.snapshot.paramMap.get('id')).subscribe((orders) => {
-      this.orders = orders;
+		this.clientesService.getSum(this.activedRoute.snapshot.paramMap.get('id')).subscribe((orders) => {
+			this.orders = orders;
 
-    });
+		});
 
-  }
+	}
 
-  async deposit() {
+	async deposit() {
 
-    const alert = await this.alertCtrl.create({
-      header: 'Deposito',
-      inputs: [
-        {
-          name: 'amount',
-          type: 'text',
-          label: 'Valor',
-        }
-      ],
-      buttons: [
-        {
-          text: 'cancelar',
-          handler: () => {
+		const alert = await this.alertCtrl.create({
+			header: 'Deposito',
+			inputs: [
+				{
+					name: 'amount',
+					type: 'text',
+					label: 'Valor',
+				}
+			],
+			buttons: [
+				{
+					text: 'cancelar',
+					handler: () => {
 
-          }
-        },
-        {
-          text: 'Depositar',
-          cssClass: 'success',
-          handler: (option) => {
-            if (option) {
-              console.log(this.clientesService.clientToShow)
-            }
-          }
-        }
-      ]
-    });
+					}
+				},
+				{
+					text: 'Depositar',
+					cssClass: 'success',
+					handler: (option) => {
+						if (option) {
+							console.log(this.clientesService.clientToShow)
+						}
+					}
+				}
+			]
+		});
 
-    return await alert.present();
-  }
+		return await alert.present();
+	}
 
-  async withdrawal() {
+	async withdrawal() {
 
-    const alert = await this.alertCtrl.create({
-      header: 'Retirar valor da conta',
-      inputs: [
-        {
-          name: 'amount',
-          type: 'text',
-          label: 'Valor',
-        }
-      ],
-      buttons: [
-        {
-          text: 'cancelar',
-          handler: () => {
+		const alert = await this.alertCtrl.create({
+			header: 'Retirar valor da conta',
+			inputs: [
+				{
+					name: 'amount',
+					type: 'text',
+					label: 'Valor',
+				}
+			],
+			buttons: [
+				{
+					text: 'cancelar',
+					handler: () => {
 
-          }
-        },
-        {
-          text: 'Retirar',
-          cssClass: 'success',
-          handler: (option) => {
-            if (option) {
-              console.log(this.clientesService.clientToShow)
-            }
-          }
-        }
-      ]
-    });
+					}
+				},
+				{
+					text: 'Retirar',
+					cssClass: 'success',
+					handler: (option) => {
+						if (option) {
+							console.log(this.clientesService.clientToShow)
+						}
+					}
+				}
+			]
+		});
 
-    return await alert.present();
-  }
+		return await alert.present();
+	}
 }

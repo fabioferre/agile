56fffdc3f6fe3d4747e17422771eb1659c6cea1d
Fabio <fabio.ferreira@w1finance.com.br>
2019-06-19 01:47:56 -0300

    fix maratona

--- src/app/pages/clientes/show-cliente/statements-cliente/statements-cliente.component.ts
+++ src/app/pages/clientes/show-cliente/statements-cliente/statements-cliente.component.ts
@@ -1,63 +1,81 @@
+import { Controller } from 'src/app/service/controller';
 import { HelperService } from './../../../../service/helper.service';
 import { PedidosService } from './../../../pedidos/pedidos.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort } from '@angular/material';
 import { ClientesService } from '../../clientes.service';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { StatementsModalComponent } from '../statements-modal/statements-modal.component';
 import { ImpressoraService } from 'src/app/pages/sistema/impressora.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-statements-cliente',
   templateUrl: './statements-cliente.component.html',
   styleUrls: ['./statements-cliente.component.scss'],
 })
-export class StatementsClienteComponent implements OnInit {
+export class StatementsClienteComponent extends Controller implements OnInit {
   displayedColumns: string[] = ['name', 'created_at', 'amount', 'action'];
-  dataSource: any
   public statements;
+
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public clientesService: ClientesService,
+  public form: FormGroup = this.fb.group({
+    start: [this.helper.date(null, "-3 month")],
+    end: [this.helper.date(), [Validators.required, Validators.minLength(2)]],
+
+  });
+
+
+  constructor(
+    public alertCtrl: AlertController,
+    private fb: FormBuilder,
+    public clientesService: ClientesService,
     public pedidosService: PedidosService,
     public modalCtrl: ModalController,
     public impressora: ImpressoraService,
-    public helper: HelperService) { }
+    public helper: HelperService) { super(alertCtrl) }
 
   ngOnInit() {
+    console.log(this.helper.date(null, "30 day"))
+    this.dataSource.sort = this.sort;
     this.impressora.getOptions().then(res => {
       this.impressora.printer_options = res;
     });
 
-    this.clientesService.statement(this.clientesService.clientToShow.account.id).subscribe((statement) => {
-      this.statements = statement;
-      this.dataSource = new MatTableDataSource<any>(this.statements);
-      console.log( this.statements)
-      this.dataSource.sort = this.sort;
+    this.getStatement();
+
+  }
+
+
+  getStatement() {
+
+    this.clientesService.statement(this.clientesService.clientToShow.id, {
+      filter: [
+        ['created_at', '>=', this.form.value.start],
+        ['created_at', '<=', this.form.value.end]
+      ]
+    }).subscribe((statement) => {
+      this.updateDataTable(statement)
+
     });
 
   }
 
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-  }
-
- async show(element) {
+  async show(element) {
     let modal = await this.modalCtrl.create({
       component: StatementsModalComponent,
-      componentProps:element
+      componentProps: element
     });
 
     return await modal.present();
 
   }
 
-  printer(){
-    
-    this.impressora.printerStatement(this.statements).subscribe(response =>{
-      if(response){
+  printer() {
+
+    this.impressora.printerStatement(this.statements).subscribe(response => {
+      if (response) {
         console.log(response)
         this.helper.message("Impressão efetuada !")
 
---
+++ src/app/pages/fretes/ne-fretes/ne-fretes.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HelperService } from 'src/app/service/helper.service';
+import { Router } from '@angular/router';
+import { FretesService } from '../fretes.service';
+
+@Component({
+  selector: 'app-ne-fretes',
+  templateUrl: './ne-fretes.component.html',
+  styleUrls: ['./ne-fretes.component.scss'],
+})
+export class NeFretesComponent implements OnInit {
+  public frete = this.fretesService.freteToEdit;
+
+  public form: FormGroup = this.fb.group({
+    neighborhood: ['', Validators.required],
+    freight: [0, Validators.required],
+
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private helper: HelperService,
+    private router: Router,
+    public fretesService: FretesService
+  ) { }
+
+  ngOnInit() {
+    this.fretesService.checkNE();
+    if(this.fretesService.elementToedit){
+      this.form.patchValue(this.fretesService.elementToedit);
+    }
+   
+  }
+
+
+  public save() {
+    this.fretesService.create(this.form.value)
+      .subscribe((frete) => {
+        if (frete) {
+          this.helper.message('Frete cadastrado')
+          this.fretesService.fretes.push(frete)
+          this.router.navigate(['/fretes']);
+        }
+      }, error => this.helper.message(error));
+
+  }
+
+  public update() {
+    this.fretesService.updateById(this.fretesService.elementToedit.id, this.form.value)
+      .subscribe((frete) => {
+        if (frete) {
+          const idx = this.fretesService.fretes.indexOf(this.fretesService.elementToedit);
+          this.fretesService.fretes[idx] =frete;
+          this.helper.message('Edição efetuada com exito')
+          this.router.navigate(['/fretes']);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.fretesService.elementToedit = null;
+    this.fretesService.ne = false;
+}
+
+}
